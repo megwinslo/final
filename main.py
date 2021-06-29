aab476f350d41d0ff5d21a83089d4c919acbff98
megwinslo <86667475+megwinslo@users.noreply.github.com>
2021-06-29 09:20:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ The main function calls all the other functions, while splitting them into diffe
 add background images and change font colors
 """
 
+pip install streamlit
 import csv
 import statistics
 import matplotlib.pyplot as plt
